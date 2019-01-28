4c8dba1c9dace8b86e6f3d925da846f8f6364315
premanandaroutray <39159009+premanandaroutray@users.noreply.github.com>
2019-01-28 16:40:08 +0530

    commited

---
+++ Attra.EWallet/Attra.EWallet/LandingPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Attra.EWallet
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LandingPage : ContentPage
+	{
+		public LandingPage ()
+		{
+			InitializeComponent ();
+		}
+
+        private async void Logout_OnClicked(object sender, EventArgs e)
+        {
+            //  var model = new LoginViewModel();
+            //  this.BindingContext = model;
+
+          
+                await Navigation.PushModalAsync(new LoginPage());
+
+        }
+    }
+}
