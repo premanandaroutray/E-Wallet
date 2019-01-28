b5e5646a20f7e1355d9939de0fccd4e566c15474
abhishek.kumar1 <send2abhishek@live.com>
2019-01-28 19:14:03 +0530

    initail checkings

--- Attra.EWallet/Attra.EWallet/App.xaml.cs
+++ Attra.EWallet/Attra.EWallet/App.xaml.cs
@@ -11,7 +11,7 @@ namespace Attra.EWallet
         {
             InitializeComponent();
 
-            MainPage = new LoginPage();
+            //MainPage = new LoginPage();
 
            
         }
